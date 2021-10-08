6f7c33dd88348d78f3411c0e5dd1b021e81638e9
Jean Duong <bduong@mymail.mines.edu>
2021-10-08 09:00:33 -0600

    orginzation

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -1,13 +1,13 @@
-﻿using GOT.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using WebUI.Models;
 
-namespace GOT.Controllers
+namespace WebUI.Controllers
 {
     public class HomeController : Controller
     {
